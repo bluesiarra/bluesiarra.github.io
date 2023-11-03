a68d1c7dfdabb732d91f4295277aea228a7c4e68
LIJAStudios <136200440+bluesiarra@users.noreply.github.com>
2023-11-03 12:37:20 -0700

    fixed toggle feature
    - working toggle feature
    - renamed main.html to index.html
    - changed styling

--- logic.js
+++ logic.js
@@ -1,6 +1,14 @@
 console.log("! WARNING !");
 console.log("Do not type anything here unless you are absolutely sure you understand what you are doing.");
 
+function all_closed(){
+    //Sets all of the unit dropdowns to be closed by default
+    elements = document.getElementsByClassName("unit-lessons");
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display="none";
+    }
+}
+
 function toggle_close(unit){
     var unit_lessons = document.getElementById("unit" + unit.toString() + "-lessons");
     if (unit_lessons.style.display === "none") {
@@ -9,3 +17,5 @@ function toggle_close(unit){
     unit_lessons.style.display = "none";
    }
 }
+
+all_closed()
